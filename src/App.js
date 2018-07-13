eb1fb3f6d42d944682a638ec541c2268e4dc3377
Rohit Vhora <rohit.anil.vhora@sprint.com>
2018-07-13 19:44:29 +0530

    Commit before eject command

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
+//import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -100,7 +100,7 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
+      //<StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
@@ -109,9 +109,10 @@ class App extends Component {
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         { persons }
       </div>
-      </StyleRoot>
+      //</StyleRoot>
     );
   }
 }
 
-export default Radium(App);
+//export default Radium(App);
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Person.css';
-import Radium from 'radium';
+//import Radium from 'radium';
 
 const person = (props) => {
-    const style = {
-        '@media (min-width: 500px)' : {
-            width: '450px'
-        }
-    }
+    // const style = {
+    //     '@media (min-width: 500px)' : {
+    //         width: '450px'
+    //     }
+    // }
     return (
         <div className="Person" style={style}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
@@ -17,4 +17,5 @@ const person = (props) => {
     )
 }
 
-export default Radium(person);
+export default person;
+//export default Radium(person);
