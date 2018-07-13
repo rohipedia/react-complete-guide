699d38c9b77a0fdac0d5106c40ec2b1f52d565eb
Rohit Vhora <rohit.anil.vhora@sprint.com>
2018-07-13 21:05:58 +0530

    CSS modules changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 //import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
@@ -55,20 +55,21 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    }
+    // const style = {
+    //   backgroundColor: 'green',
+    //   color: 'white',
+    //   font: 'inherit',
+    //   border: '1px solid blue',
+    //   padding: '8px',
+    //   cursor: 'pointer',
+    //   ':hover': {
+    //     backgroundColor: 'lightgreen',
+    //     color: 'black'
+    //   }
+    // }
 
     let persons = null;
+    let btnClass = null;
 
     if (this.state.showPersons) {
       persons = (
@@ -82,30 +83,31 @@ class App extends Component {
               changed={(event) => this.nameChangedHandler(event, person.id)}/>
           })}
         </div>
-      )
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
+      );
+      btnClass = classes.Red;
+      // style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'black'
+      // }
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red'); //class='red'
+      assignedClasses.push( classes.red ); //class='red'
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push('bold'); //class='bold red'
+      assignedClasses.push( classes.bold ); //class='bold red'
     }
 
     return (
-      //<StyleRoot>
-      <div className="App">
+      //<StyleRoot> //style={style} <- on button
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button
-          style={style}
+          className={btnClass}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         { persons }
       </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 //import Radium from 'radium';
 
 const person = (props) => {
@@ -9,7 +9,8 @@ const person = (props) => {
     //     }
     // }
     return (
-        <div className="Person" style={style}>
+        //style={style}
+        <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
